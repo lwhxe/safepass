43da2803773aae5aa46504358dbbe3c217a0263a
lwhxe <alexanderthomasbohman8@gmail.com>
2025-09-12 15:51:17 +0200

    help screen

--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,16 @@
 const std = @import("std");
 
-pub fn main() void {}
+fn help() void {
+    std.debug.print("HELP SCREEN HERE\n", .{});
+}
+
+pub fn main() !void {
+    const args = try std.process.argsAlloc(std.heap.page_allocator);
+    if (args.len == 0) {
+        help();
+    } else if (std.mem.eql(u8, args[1], "help")) {
+        help();
+    }
+
+
+}
